da2fbc6668b812e1c1a09a572eff609cde213b88
Nitik Ranjan <ranjannitik0611@gmail.com>
2024-07-17 23:58:25 +0530

    "Done Day2"

---
+++ Day2.js
@@ -0,0 +1,95 @@
+// Activity 1 : Arithmetic Operations
+
+// Task 1
+console.log("Task 1")
+let a = 4
+let b = 2
+console.log(a + b, "\n")
+
+// Task 2
+console.log("Task 2")
+console.log(a - b, "\n")
+
+// Task 3
+console.log("Task 3")
+console.log(a * b, "\n")
+
+// Task 4
+console.log("Task 4")
+console.log(a / b, "\n")
+
+// Task 5
+console.log("Task 5")
+console.log(a % b, "\n")
+
+// Activity 2 Assignment Operators
+
+// Task 6
+console.log("Task 6")
+let c = 10
+let d = 5
+c += d
+console.log(c, "\n")
+
+// Task 7
+console.log("Task 7")
+c -= d
+console.log(c, "\n")
+
+// Activity 3 Comparison Operators
+
+// Task 8
+console.log("Task 8")
+let e = 10
+let f = 10
+console.log(e > f, "\n")
+console.log(e < f, "\n")
+
+// Task 9
+console.log("Task 9")
+console.log(e >= f, "\n")
+console.log(e <= f, "\n")
+
+// Task 10
+console.log("Task 10")
+let name = "92"
+let no = 92
+console.log("\n")
+console.log(name == no, "\n")
+console.log(name === no, "\n")
+
+// Activity 4 Logical Operators
+
+// Task 11
+console.log("Task 11")
+let age = 12
+let qage = 10
+let Ans = age > 12 && qage == 10
+console.log(Ans, "\n")
+
+// Task 12
+console.log("Task 12")
+let Ans2 = age > 12 || qage == 10
+console.log(Ans2, "\n")
+
+// Task 13
+console.log("Task 13")
+console.log(age != qage, "\n")
+
+// Activity 5 Ternary Operator
+
+// Task 14
+console.log("Task 14 \n")
+let num = -32
+let pro = num > 0 ? true : false
+console.log(pro, "\n")
+
+// Summary and Feelings
+console.log("Code By Nitik ")
+console.log("- I feel happy and excited to have completed a lot of lessons quickly!")
+console.log("- This exercise was a fast and effective revision of various JavaScript concepts:")
+console.log("  - Arithmetic operations")
+console.log("  - Assignment operators")
+console.log("  - Comparison operators")
+console.log("  - Logical operators")
+console.log("  - Ternary operator")
